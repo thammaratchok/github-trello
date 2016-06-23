cc728fc81409eae1342cef2a8525338d2b6e0ddd
gen chokwattanakit <genchokwattanakit@gens-MacBook-Pro-3.local>
2016-06-23 19:46:10 +0700

    Revert for branch created

--- lib/github-trello/server.rb
+++ lib/github-trello/server.rb
@@ -17,63 +17,64 @@ module GithubTrello
         return
       end
 
-      #branch = payload["ref"].gsub("refs/heads/", "")
-      # if config["blacklist_branches"] and config["blacklist_branches"].include?(branch)
-      #   return
-      # elsif config["whitelist_branches"] and !config["whitelist_branches"].include?(branch)
-      #   return
-      # end
-
-      payload["pull_request"].each do |pr|
-        # Figure out the card short id
-        match = pr["title"].match(/((case|card|close|archive|fix)e?s?\D?([0-9]+))/i)
-        next unless match and match[3].to_i > 0
-
-        results = http.get_card(board_id, match[3].to_i)
-        unless results
-          puts "[ERROR] Cannot find card matching ID #{match[3]}"
-          next
-        end
-
-        results = JSON.parse(results)
-
-        # Add the commit comment
-        #message = "#{commit["author"]["name"]}: #{commit["message"]}\n\n[#{branch}] #{commit["url"]}"
-        #message.gsub!(match[1], "")
-        #message.gsub!(/\(\)$/, "")
-        message = "PR created"
-        http.add_comment(results["id"], message)
-
-        # Determine the action to take
-        update_config = case match[2].downcase
-          when "case", "card" then config["on_start"]
-          when "close", "fix" then config["on_close"]
-          when "archive" then {:archive => true}
-        end
-
-        next unless update_config.is_a?(Hash)
-
-        # Modify it if needed
-        to_update = {}
-
-        if update_config["move_to"].is_a?(Hash)
-          move_to = update_config["move_to"][payload["repository"]["name"]]
-        else
-          move_to = update_config["move_to"]
-        end
-
-        unless results["idList"] == move_to
-          to_update[:idList] = move_to
-        end
-
-        if !results["closed"] and update_config["archive"]
-          to_update[:closed] = true
-        end
-
-        unless to_update.empty?
-          http.update_card(results["id"], to_update)
-        end
+      branch = payload["ref"].gsub("refs/heads/", "")
+      if config["blacklist_branches"] and config["blacklist_branches"].include?(branch)
+        return
+      elsif config["whitelist_branches"] and !config["whitelist_branches"].include?(branch)
+        return
       end
+
+      #payload["commits"].each do |commit|
+      # Figure out the card short id
+      match = branch.match(/((case|card|close|archive|fix)e?s?\D?([0-9]+))/i)
+      next unless match and match[3].to_i > 0
+
+      results = http.get_card(board_id, match[3].to_i)
+      unless results
+        puts "[ERROR] Cannot find card matching ID #{match[3]}"
+        next
+      end
+
+      results = JSON.parse(results)
+
+      # Add the commit comment
+      #message = "#{commit["author"]["name"]}: #{commit["message"]}\n\n[#{branch}] #{commit["url"]}"
+      #message.gsub!(match[1], "")
+      #message.gsub!(/\(\)$/, "")
+      message = "new branch created"
+
+      http.add_comment(results["id"], message)
+
+      # Determine the action to take
+      update_config = case match[2].downcase
+        when "case", "card" then config["on_start"]
+        when "close", "fix" then config["on_close"]
+        when "archive" then {:archive => true}
+      end
+
+      next unless update_config.is_a?(Hash)
+
+      # Modify it if needed
+      to_update = {}
+
+      if update_config["move_to"].is_a?(Hash)
+        move_to = update_config["move_to"][payload["repository"]["name"]]
+      else
+        move_to = update_config["move_to"]
+      end
+
+      unless results["idList"] == move_to
+        to_update[:idList] = move_to
+      end
+
+      if !results["closed"] and update_config["archive"]
+        to_update[:closed] = true
+      end
+
+      unless to_update.empty?
+        http.update_card(results["id"], to_update)
+      end
+      #end
 
       ""
     end
