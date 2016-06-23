40e43abf47ba59880e50eba79ae694b921e08928
gen chokwattanakit <genchokwattanakit@gens-MacBook-Pro-3.local>
2016-06-23 15:59:09 +0700

    get branch instead

--- lib/github-trello/server.rb
+++ lib/github-trello/server.rb
@@ -26,7 +26,7 @@ module GithubTrello
        # ["commits"].each do |commit|
        ref = ["ref"]
         # Figure out the card short id
-        match = ref["ref"].match(/((case|card|close|archive|fix)e?s? \D?([0-9]+))/i)
+        match = ref["ref"].match(/((case|card|close|archive|fix)e?s?\D?([0-9]+))/i)
         next unless match and match[3].to_i > 0
 
         results = http.get_card(board_id, match[3].to_i)
