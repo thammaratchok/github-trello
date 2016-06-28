a1bcffea909160010dfc4ab6dda0c40aa90fd634
gen chokwattanakit <genchokwattanakit@gens-MacBook-Pro-3.local>
2016-06-28 15:14:14 +0700

    adjust match with card id

--- lib/github-trello/server.rb
+++ lib/github-trello/server.rb
@@ -27,11 +27,11 @@ module GithubTrello
       pr = payload["pull_request"]
       # Figure out the card short id
       match = pr["title"].match(/(\D?([0-9]+))/i)
-      next unless match and match[3].to_i > 0
+      next unless match and match[2].to_i > 0
 
-      results = http.get_card(board_id, match[3].to_i)
+      results = http.get_card(board_id, match[2].to_i)
       unless results
-        puts "[ERROR] Cannot find card matching ID #{match[3]}"
+        puts "[ERROR] Cannot find card matching ID #{match[2]}"
         next
       end
 
